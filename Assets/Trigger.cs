6db03040907243cf0023b5f4ca133980eb5b88b0
shadowgostl <117022084+Gyroooooo@users.noreply.github.com>
2022-11-06 15:36:55 +0200

    Clean Up XR

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -6,10 +6,11 @@ using UnityEngine.Events;
 public class Trigger : MonoBehaviour
 {
   public UnityEvent custom_on_trigger_enter;
+	
 	 void OnTriggerEnter()
 	{
 		custom_on_trigger_enter.Invoke();
 
-       print ("Ball touch me")
+       print ("Ball touch me");
 	}
 }
