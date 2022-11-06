94223e994f67c8017589a1d713a5646b6c83c911
shadowgostl <117022084+Gyroooooo@users.noreply.github.com>
2022-11-06 17:53:47 +0200

    Piza mocarella

---
+++ Assets/Gyro/Trigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+  public UnityEvent custom_on_trigger_enter;
+	
+	 void OnTriggerEnter()
+	{
+		custom_on_trigger_enter.Invoke();
+
+       print ("Ball touch me");
+	}
+}
